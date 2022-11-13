0af703675ce5807d1bec9761e6b87a623be5fc27
David Chanin <chanindav@gmail.com>
2022-11-13 00:33:50 +0000

    porting tree code

---
+++ src/lib/tree.spec.ts
@@ -0,0 +1,310 @@
+import test from 'ava';
+
+import { _default_variable_prefix, is_atomic, Tree } from './tree';
+import { Node } from './types';
+
+const empty_node = (): Node => ['a'];
+
+const simple_node = (): Node => ['a', [['/', 'alpha']]];
+
+const one_arg_node = (): Node => [
+  'a',
+  [
+    ['/', 'alpha'],
+    [':ARG0', ['b', [['/', 'beta']]]],
+  ],
+];
+
+const reentrant = (): Node => [
+  'a',
+  [
+    ['/', 'alpha'],
+    [':ARG0', ['b', [['/', 'beta']]]],
+    [
+      ':ARG1',
+      [
+        'g',
+        [
+          ['/', 'gamma'],
+          [':ARG0', 'b'],
+        ],
+      ],
+    ],
+  ],
+];
+
+const var_instance = (): Node => [
+  'a',
+  [
+    ['/', 'alpha'],
+    [':ARG0', ['b', [['/', 'b']]]],
+  ],
+];
+
+test('_init__', (t) => {
+  // @ts-ignore
+  t.throws(() => new Tree());
+  const t1 = new Tree(empty_node());
+  t.deepEqual(t1.node, empty_node());
+  t.deepEqual(t1.metadata, {});
+
+  const t2 = new Tree(simple_node(), { snt: 'Alpha.' });
+  t.deepEqual(t2.node, simple_node());
+  t.deepEqual(t2.metadata, { snt: 'Alpha.' });
+});
+
+//     def test_nodes(self, one_arg_node, reentrant):
+//         t = tree.Tree(one_arg_node)
+//         assert t.nodes() == [one_arg_node, ('b', [('/', 'beta')])]
+
+//         t = tree.Tree(reentrant)
+//         assert t.nodes() == [reentrant,
+//                              ('b', [('/', 'beta')]),
+//                              ('g', [('/', 'gamma'), (':ARG0', 'b')])]
+
+test('nodes', (t) => {
+  const t1 = new Tree(one_arg_node());
+  t.deepEqual(t1.nodes(), [one_arg_node(), ['b', [['/', 'beta']]]]);
+
+  const t2 = new Tree(reentrant());
+  t.deepEqual(t2.nodes(), [
+    reentrant(),
+    ['b', [['/', 'beta']]],
+    [
+      'g',
+      [
+        ['/', 'gamma'],
+        [':ARG0', 'b'],
+      ],
+    ],
+  ]);
+});
+
+//     def test_walk(self, one_arg_node, reentrant):
+//         t = tree.Tree(one_arg_node)
+//         assert list(t.walk()) == [
+//             ((0,), ('/', 'alpha')),
+//             ((1,), (':ARG0', ('b', [('/', 'beta')]))),
+//             ((1, 0), ('/', 'beta')),
+//         ]
+
+//         t = tree.Tree(reentrant)
+//         assert list(t.walk()) == [
+//             ((0,), ('/', 'alpha')),
+//             ((1,), (':ARG0', ('b', [('/', 'beta')]))),
+//             ((1, 0), ('/', 'beta')),
+//             ((2,), (':ARG1', ('g', [('/', 'gamma'),
+//                                     (':ARG0', 'b')]))),
+//             ((2, 0), ('/', 'gamma')),
+//             ((2, 1), (':ARG0', 'b')),
+//         ]
+
+//         t = tree.Tree(('a', [('/', 'alpha'),
+//                              (':polarity', '-'),
+//                              (':ARG0', ('b', [('/', 'beta')]))]))
+//         assert list(t.walk()) == [
+//             ((0,), ('/', 'alpha')),
+//             ((1,), (':polarity', '-')),
+//             ((2,), (':ARG0', ('b', [('/', 'beta')]))),
+//             ((2, 0), ('/', 'beta')),
+//         ]
+
+test('walk', (t) => {
+  const t1 = new Tree(one_arg_node());
+  t.deepEqual(Array.from(t1.walk()), [
+    [[0], ['/', 'alpha']],
+    [[1], [':ARG0', ['b', [['/', 'beta']]]]],
+    [
+      [1, 0],
+      ['/', 'beta'],
+    ],
+  ]);
+
+  const t2 = new Tree(reentrant());
+  t.deepEqual(Array.from(t2.walk()), [
+    [[0], ['/', 'alpha']],
+    [[1], [':ARG0', ['b', [['/', 'beta']]]]],
+    [
+      [1, 0],
+      ['/', 'beta'],
+    ],
+    [
+      [2],
+      [
+        ':ARG1',
+        [
+          'g',
+          [
+            ['/', 'gamma'],
+            [':ARG0', 'b'],
+          ],
+        ],
+      ],
+    ],
+    [
+      [2, 0],
+      ['/', 'gamma'],
+    ],
+    [
+      [2, 1],
+      [':ARG0', 'b'],
+    ],
+  ]);
+
+  const t3 = new Tree([
+    'a',
+    [
+      ['/', 'alpha'],
+      [':polarity', '-'],
+      [':ARG0', ['b', [['/', 'beta']]]],
+    ],
+  ]);
+  t.deepEqual(Array.from(t3.walk()), [
+    [[0], ['/', 'alpha']],
+    [[1], [':polarity', '-']],
+    [[2], [':ARG0', ['b', [['/', 'beta']]]]],
+    [
+      [2, 0],
+      ['/', 'beta'],
+    ],
+  ]);
+});
+
+//     def test_reset_variables(self, one_arg_node, reentrant, var_instance):
+
+//         def _vars(t):
+//             return [v for v, _ in t.nodes()]
+
+//         t = tree.Tree(one_arg_node)
+//         assert _vars(t) == ['a', 'b']
+
+//         t.reset_variables(fmt='a{i}')
+//         assert _vars(t) == ['a0', 'a1']
+
+//         t.reset_variables(fmt='a{j}')
+//         assert _vars(t) == ['a', 'a2']
+
+//         t.reset_variables()
+//         assert _vars(t) == ['a', 'b']
+
+//         t = tree.Tree(reentrant)
+//         assert _vars(t) == ['a', 'b', 'g']
+
+//         t.reset_variables(fmt='a{i}')
+//         assert _vars(t) == ['a0', 'a1', 'a2']
+//         assert t == (
+//             'a0', [('/', 'alpha'),
+//                    (':ARG0', ('a1', [('/', 'beta')])),
+//                    (':ARG1', ('a2', [('/', 'gamma'),
+//                                      (':ARG0', 'a1')]))])
+
+//         t.reset_variables()
+//         assert _vars(t) == ['a', 'b', 'g']
+
+//         t = tree.Tree(var_instance)
+//         assert _vars(t) == ['a', 'b']
+
+//         t.reset_variables(fmt='a{i}')
+//         assert _vars(t) == ['a0', 'a1']
+//         assert t == (
+//             'a0', [('/', 'alpha'),
+//                    (':ARG0', ('a1', [('/', 'b')]))])
+
+//         t.reset_variables()
+//         assert _vars(t) == ['a', 'b']
+
+test('reset_variables', (t) => {
+  const _vars = (t) => t.nodes().map(([v]) => v);
+
+  const t1 = new Tree(one_arg_node());
+  t.deepEqual(_vars(t1), ['a', 'b']);
+
+  t1.reset_variables('a{i}');
+  t.deepEqual(_vars(t1), ['a0', 'a1']);
+
+  t1.reset_variables('a{j}');
+  t.deepEqual(_vars(t1), ['a', 'a2']);
+
+  t1.reset_variables();
+  t.deepEqual(_vars(t1), ['a', 'b']);
+
+  const t2 = new Tree(reentrant());
+  t.deepEqual(_vars(t2), ['a', 'b', 'g']);
+
+  t2.reset_variables('a{i}');
+  t.deepEqual(_vars(t2), ['a0', 'a1', 'a2']);
+  t.deepEqual(
+    t2,
+    new Tree([
+      'a0',
+      [
+        ['/', 'alpha'],
+        [':ARG0', ['a1', [['/', 'beta']]]],
+        [
+          ':ARG1',
+          [
+            'a2',
+            [
+              ['/', 'gamma'],
+              [':ARG0', 'a1'],
+            ],
+          ],
+        ],
+      ],
+    ])
+  );
+
+  t2.reset_variables();
+  t.deepEqual(_vars(t2), ['a', 'b', 'g']);
+
+  const t3 = new Tree(var_instance());
+  t.deepEqual(_vars(t3), ['a', 'b']);
+
+  t3.reset_variables('a{i}');
+  t.deepEqual(_vars(t3), ['a0', 'a1']);
+  t.deepEqual(
+    t3,
+    new Tree([
+      'a0',
+      [
+        ['/', 'alpha'],
+        [':ARG0', ['a1', [['/', 'b']]]],
+      ],
+    ])
+  );
+
+  t3.reset_variables();
+  t.deepEqual(_vars(t3), ['a', 'b']);
+});
+
+// def test_is_atomic():
+//     assert tree.is_atomic('a')
+//     assert tree.is_atomic(None)
+//     assert tree.is_atomic(3.14)
+//     assert not tree.is_atomic(('a', [('/', 'alpha')]))
+
+test('is_atomic', (t) => {
+  t.true(is_atomic('a'));
+  t.true(is_atomic(null));
+  t.true(is_atomic(3.14));
+  t.false(is_atomic(['a', [['/', 'alpha']]]));
+});
+
+// def test_default_variable_prefix():
+//     assert tree._default_variable_prefix('Alphabet') == 'a'
+//     assert tree._default_variable_prefix('chase-01') == 'c'
+//     assert tree._default_variable_prefix('"string"') == 's'
+//     assert tree._default_variable_prefix('_predicate_n_1"') == 'p'
+//     assert tree._default_variable_prefix(1) == '_'
+//     assert tree._default_variable_prefix(None) == '_'
+//     assert tree._default_variable_prefix('') == '_'
+
+test('default_variable_prefix', (t) => {
+  t.is(_default_variable_prefix('Alphabet'), 'a');
+  t.is(_default_variable_prefix('chase-01'), 'c');
+  t.is(_default_variable_prefix('"string"'), 's');
+  t.is(_default_variable_prefix('_predicate_n_1"'), 'p');
+  t.is(_default_variable_prefix(1), '_');
+  t.is(_default_variable_prefix(null), '_');
+  t.is(_default_variable_prefix(''), '_');
+});
---
+++ src/lib/tree.ts
@@ -0,0 +1,232 @@
+/**
+Definitions of tree structures.
+*/
+
+import format from 'string-format';
+
+import type { Branch, Node, Variable } from './types';
+
+type _Step = [number[], Branch]; // see Tree.walk()
+type VarMap = { [key: string]: string };
+
+export class Tree {
+  /**
+    A tree structure.
+
+    A tree is essentially a node that contains other nodes, but this
+    Tree class is useful to contain any metadata and to provide
+    tree-based methods.
+     */
+
+  node: Node;
+  metadata: { [key: string]: string };
+
+  constructor(node: Node, metadata?: { [key: string]: string }) {
+    if (!node) {
+      throw new Error('Tree node cannot be null');
+    }
+    this.node = node;
+    this.metadata = metadata ?? {};
+  }
+
+  __eq__(other: any): boolean {
+    if (other instanceof Tree) {
+      other = other.node;
+    }
+    return this.node === other;
+  }
+  __repr__(): string {
+    return `Tree(${this.node})`;
+  }
+  __str__(): string {
+    const s = _format(this.node, 2);
+    return `Tree(\n  ${s})`;
+  }
+  nodes(): Node[] {
+    /**
+     Return the nodes in the tree as a flat list.
+     */
+    return _nodes(this.node);
+  }
+
+  *walk(): Generator<_Step> {
+    /**
+        Iterate over branches in the tree.
+
+        This function yields pairs of (*path*, *branch*) where each
+        *path* is a tuple of 0-based indices of branches to get to
+        *branch*. For example, the path (2, 0) is the concept branch
+        `('/', 'bark-01')` in the tree for the following PENMAN
+        string, traversing first to the third (index 2) branch of the
+        top node, then to the first (index 0) branch of that node::
+
+            (t / try-01
+               :ARG0 (d / dog)
+               :ARG1 (b / bark-01
+                        :ARG0 d))
+
+        The (*path*, *branch*) pairs are yielded in depth-first order
+        of the tree traversal.
+         */
+    yield* _walk(this.node, []);
+  }
+  reset_variables(fmt = '{prefix}{j}'): void {
+    /**
+    Recreate node variables formatted using *fmt*.
+
+    The *fmt* string can be formatted with the following values:
+
+    - ``prefix``: first alphabetic character in the node's concept
+    - ``i``: 0-based index of the current occurrence of the prefix
+    - ``j``: 1-based index starting from the second occurrence
+     */
+    const varmap: VarMap = {};
+    const used: Set<Variable> = new Set();
+    for (const node of this.nodes()) {
+      const [variable, branches] = node;
+      if (!(variable in varmap)) {
+        const concept = branches.find((branch) => branch[0] === '/')?.[1];
+        const pre = _default_variable_prefix(concept);
+        let i = 0;
+        let newvar: string | null = null;
+        while (newvar === null || used.has(newvar)) {
+          newvar = format(fmt, {
+            prefix: pre,
+            i: i,
+            j: i === 0 ? '' : `${i + 1}`,
+          });
+          i += 1;
+        }
+        used.add(newvar);
+        varmap[variable] = newvar;
+      }
+    }
+    this.node = _map_vars(this.node, varmap);
+  }
+}
+
+const _format = (node: Node, level: number): string => {
+  const [variable, branches] = node;
+  const next_level = level + 2;
+  const indent = '\n' + ' '.repeat(next_level);
+  const branch_strings = branches.map((branch) =>
+    _format_branch(branch, next_level)
+  );
+  return format(
+    '({}, [{}{}])',
+    variable,
+    indent,
+    branch_strings.join(',' + indent)
+  );
+};
+
+const _format_branch = (branch: Branch, level: number): string => {
+  const [role, target] = branch;
+  const target_str = is_atomic(target) ? `${target}` : _format(target, level);
+  return `(${role}, ${target_str})`;
+};
+
+const _nodes = (node: Node): Node[] => {
+  const [variable, branches] = node;
+  let ns = variable ? [node] : [];
+  for (const branch of branches) {
+    const target = branch[1];
+    // if target is not atomic, assume it's a valid tree node
+    if (!is_atomic(target)) {
+      ns = ns.concat(_nodes(target));
+    }
+  }
+  return ns;
+};
+
+function* _walk(node: Node, path: number[]) {
+  const branches = node[1];
+  for (const [i, branch] of branches.entries()) {
+    const curpath = path.concat([i]);
+    yield [curpath, branch];
+    const target = branch[1];
+    if (!is_atomic(target)) {
+      yield* _walk(target, curpath);
+    }
+  }
+}
+
+export const _default_variable_prefix = (concept: any): Variable => {
+  /**
+    Return the variable prefix for *concept*.
+
+    If *concept* is a non-empty string, the prefix is the first
+    alphabetic character in the string, if there are any, downcased.
+    Otherwise the prefix is ``'_'``.
+
+    Examples:
+        >>> _default_variable_prefix('Alphabet')
+        'a'
+        >>> _default_variable_prefix('chase-01')
+        'c'
+        >>> _default_variable_prefix('"string"')
+        's'
+        >>> _default_variable_prefix('_predicate_n_1"')
+        'p'
+        >>> _default_variable_prefix(1)
+        '_'
+        >>> _default_variable_prefix(None)
+        '_'
+        >>> _default_variable_prefix('')
+        '_'
+     */
+  let prefix = '_';
+  if (concept && typeof concept === 'string') {
+    for (const c of concept) {
+      if (isalpha(c)) {
+        prefix = c.toLowerCase();
+        break;
+      }
+    }
+  }
+  return prefix;
+};
+
+function isalpha(c: string): boolean {
+  return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+}
+
+const _map_vars = (node: Node, varmap: VarMap): Node => {
+  const [variable, branches] = node;
+
+  const newbranches: Branch[] = [];
+  for (const branch of branches) {
+    const [role, target] = branch;
+    let tgt = target;
+    if (!is_atomic(tgt)) {
+      tgt = _map_vars(tgt, varmap);
+    } else if (role !== '/' && tgt in varmap) {
+      tgt = varmap[tgt];
+    }
+    newbranches.push([role, tgt]);
+  }
+  return [varmap[variable], newbranches];
+};
+
+export const is_atomic = (x: any): boolean => {
+  /**
+    Return ``True`` if *x* is a valid atomic value.
+
+    Examples:
+        >>> from penman.tree import is_atomic
+        >>> is_atomic('a')
+        True
+        >>> is_atomic(None)
+        True
+        >>> is_atomic(3.14)
+        True
+        >>> is_atomic(('a', [('/', 'alpha')]))
+        False
+     */
+  return (
+    x === null ||
+    x === undefined ||
+    typeof x === 'string' ||
+    typeof x === 'number'
+  );
+};
