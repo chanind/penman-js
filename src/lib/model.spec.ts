d994b133d05720674cb7197bd358dafe625944bc
David Chanin <chanindav@gmail.com>
2022-12-20 15:20:41 -0500

    porting adding model.ts

---
+++ src/lib/model.spec.ts
@@ -0,0 +1,351 @@
+import test from 'ava';
+
+import { Graph } from './graph';
+import { Model } from './model';
+import { BasicTriple } from './types';
+
+const miniAmr = (): {
+  roles: { [key: string]: { type: string } };
+  normalizations: { [key: string]: string };
+  reifications: [string, string, string, string][];
+} => ({
+  roles: {
+    ':ARG0': { type: 'frame' },
+    ':ARG1': { type: 'frame' },
+    ':accompanier': { type: 'general' },
+    ':domain': { type: 'general' },
+    ':consist-of': { type: 'general' },
+    ':mod': { type: 'general' },
+    ':op[0-9]+': { type: 'op' },
+  },
+  normalizations: {
+    ':mod-of': ':domain',
+    ':domain-of': ':mod',
+  },
+  reifications: [
+    [':accompanier', 'accompany-01', ':ARG0', ':ARG1'],
+    [':mod', 'have-mod-91', ':ARG1', ':ARG2'],
+  ],
+});
+
+test('init', (t) => {
+  const m = new Model();
+  t.is(Object.keys(m.roles).length, 0);
+  const m1 = new Model(undefined, undefined, undefined, miniAmr().roles);
+  t.is(Object.keys(m1.roles).length, 7);
+});
+
+test('from_dict', (t) => {
+  t.deepEqual(
+    Model.fromDict(miniAmr()),
+    new Model(
+      undefined,
+      undefined,
+      undefined,
+      miniAmr().roles,
+      miniAmr().normalizations,
+      miniAmr().reifications
+    )
+  );
+});
+
+test('has_role', (t) => {
+  const m = new Model();
+  t.false(m.hasRole(''));
+  t.true(m.hasRole(m.conceptRole));
+  t.false(m.hasRole(':ARG0'));
+  t.false(m.hasRole(':ARG0-of'));
+  const m1 = Model.fromDict(miniAmr());
+  t.false(m1.hasRole(''));
+  t.true(m1.hasRole(m1.conceptRole));
+  t.true(m1.hasRole(':ARG0'));
+  t.true(m1.hasRole(':ARG0-of'));
+  t.true(m1.hasRole(':mod'));
+  t.true(m1.hasRole(':mod-of'));
+  t.false(m1.hasRole(':consist'));
+  t.true(m1.hasRole(':consist-of'));
+  t.true(m1.hasRole(':consist-of-of'));
+  t.false(m1.hasRole(':fake'));
+  t.true(m1.hasRole(':op1'));
+  t.true(m1.hasRole(':op10'));
+  t.true(m1.hasRole(':op9999'));
+  t.false(m1.hasRole(':op[0-9]+'));
+});
+
+test('is_role_inverted', (t) => {
+  const m = new Model();
+  t.true(m.isRoleInverted(':ARG0-of'));
+  t.true(m.isRoleInverted(':-of'));
+  t.false(m.isRoleInverted(':ARG0'));
+  t.false(m.isRoleInverted(':'));
+  t.true(m.isRoleInverted(':consist-of'));
+
+  const m1 = Model.fromDict(miniAmr());
+  t.true(m1.isRoleInverted(':mod-of'));
+  t.true(m1.isRoleInverted(':domain-of'));
+  t.false(m1.isRoleInverted(':mod'));
+  t.false(m1.isRoleInverted(':domain'));
+  t.true(m1.isRoleInverted(':consist-of-of'));
+  t.false(m1.isRoleInverted(':consist-of'));
+});
+
+test('invert_role', (t) => {
+  const m = new Model();
+  t.is(m.invertRole(':ARG0'), ':ARG0-of');
+  t.is(m.invertRole(':ARG0-of'), ':ARG0');
+  t.is(m.invertRole(':consist-of'), ':consist');
+  t.is(m.invertRole(':mod'), ':mod-of');
+  t.is(m.invertRole(':domain'), ':domain-of');
+
+  const m1 = Model.fromDict(miniAmr());
+  t.is(m1.invertRole(':ARG0'), ':ARG0-of');
+  t.is(m1.invertRole(':ARG0-of'), ':ARG0');
+  t.is(m1.invertRole(':consist-of'), ':consist-of-of');
+  t.is(m1.invertRole(':mod'), ':mod-of');
+  t.is(m1.invertRole(':domain'), ':domain-of');
+});
+
+test('invert', (t) => {
+  const m = new Model();
+  t.deepEqual(m.invert(['a', ':ARG0', 'b']), ['b', ':ARG0-of', 'a']);
+  t.deepEqual(m.invert(['a', ':ARG0-of', 'b']), ['b', ':ARG0', 'a']);
+  t.deepEqual(m.invert(['a', ':consist-of', 'b']), ['b', ':consist', 'a']);
+  t.deepEqual(m.invert(['a', ':mod', 'b']), ['b', ':mod-of', 'a']);
+  t.deepEqual(m.invert(['a', ':domain', 'b']), ['b', ':domain-of', 'a']);
+
+  const m1 = Model.fromDict(miniAmr());
+  t.deepEqual(m1.invert(['a', ':ARG0', 'b']), ['b', ':ARG0-of', 'a']);
+  t.deepEqual(m1.invert(['a', ':ARG0-of', 'b']), ['b', ':ARG0', 'a']);
+  t.deepEqual(m1.invert(['a', ':consist-of', 'b']), [
+    'b',
+    ':consist-of-of',
+    'a',
+  ]);
+  t.deepEqual(m1.invert(['a', ':mod', 'b']), ['b', ':mod-of', 'a']);
+  t.deepEqual(m1.invert(['a', ':domain', 'b']), ['b', ':domain-of', 'a']);
+});
+
+test('deinvert', (t) => {
+  const m = new Model();
+  t.deepEqual(m.deinvert(['a', ':ARG0', 'b']), ['a', ':ARG0', 'b']);
+  t.deepEqual(m.deinvert(['a', ':ARG0-of', 'b']), ['b', ':ARG0', 'a']);
+  t.deepEqual(m.deinvert(['a', ':consist-of', 'b']), ['b', ':consist', 'a']);
+  t.deepEqual(m.deinvert(['a', ':mod', 'b']), ['a', ':mod', 'b']);
+  t.deepEqual(m.deinvert(['a', ':domain', 'b']), ['a', ':domain', 'b']);
+
+  const m1 = Model.fromDict(miniAmr());
+  t.deepEqual(m1.deinvert(['a', ':ARG0', 'b']), ['a', ':ARG0', 'b']);
+  t.deepEqual(m1.deinvert(['a', ':ARG0-of', 'b']), ['b', ':ARG0', 'a']);
+  t.deepEqual(m1.deinvert(['a', ':consist-of', 'b']), [
+    'a',
+    ':consist-of',
+    'b',
+  ]);
+  t.deepEqual(m1.deinvert(['a', ':mod', 'b']), ['a', ':mod', 'b']);
+  t.deepEqual(m1.deinvert(['a', ':domain', 'b']), ['a', ':domain', 'b']);
+});
+
+test('canonicalizeRole', (t) => {
+  const m = new Model();
+  t.is(m.canonicalizeRole(':ARG0'), ':ARG0');
+  t.is(m.canonicalizeRole(':ARG0-of'), ':ARG0-of');
+  t.is(m.canonicalizeRole(':ARG0-of-of'), ':ARG0');
+  t.is(m.canonicalizeRole(':consist'), ':consist');
+  t.is(m.canonicalizeRole(':consist-of'), ':consist-of');
+  t.is(m.canonicalizeRole(':consist-of-of'), ':consist');
+  t.is(m.canonicalizeRole(':mod'), ':mod');
+  t.is(m.canonicalizeRole(':mod-of'), ':mod-of');
+  t.is(m.canonicalizeRole(':domain'), ':domain');
+  t.is(m.canonicalizeRole(':domain-of'), ':domain-of');
+  // without :
+  t.is(m.canonicalizeRole('ARG0'), ':ARG0');
+  t.is(m.canonicalizeRole('ARG0-of'), ':ARG0-of');
+  t.is(m.canonicalizeRole('ARG0-of-of'), ':ARG0');
+
+  const m1 = Model.fromDict(miniAmr());
+  t.is(m1.canonicalizeRole(':ARG0'), ':ARG0');
+  t.is(m1.canonicalizeRole(':ARG0-of'), ':ARG0-of');
+  t.is(m1.canonicalizeRole(':ARG0-of-of'), ':ARG0');
+  t.is(m1.canonicalizeRole(':consist'), ':consist-of-of');
+  t.is(m1.canonicalizeRole(':consist-of'), ':consist-of');
+  t.is(m1.canonicalizeRole(':consist-of-of'), ':consist-of-of');
+  t.is(m1.canonicalizeRole(':mod'), ':mod');
+  t.is(m1.canonicalizeRole(':mod-of'), ':domain');
+  t.is(m1.canonicalizeRole(':domain'), ':domain');
+  t.is(m1.canonicalizeRole(':domain-of'), ':mod');
+  // without :
+  t.is(m1.canonicalizeRole('consist'), ':consist-of-of');
+  t.is(m1.canonicalizeRole('consist-of'), ':consist-of');
+  t.is(m1.canonicalizeRole('consist-of-of'), ':consist-of-of');
+});
+
+test('canonicalize', (t) => {
+  const m1 = new Model();
+  t.deepEqual(m1.canonicalize(['a', ':ARG0', 'b']), ['a', ':ARG0', 'b']);
+  t.deepEqual(m1.canonicalize(['a', ':ARG0-of', 'b']), ['a', ':ARG0-of', 'b']);
+  t.deepEqual(m1.canonicalize(['a', ':ARG0-of-of', 'b']), ['a', ':ARG0', 'b']);
+  t.deepEqual(m1.canonicalize(['a', ':consist', 'b']), ['a', ':consist', 'b']);
+  t.deepEqual(m1.canonicalize(['a', ':consist-of', 'b']), [
+    'a',
+    ':consist-of',
+    'b',
+  ]);
+  t.deepEqual(m1.canonicalize(['a', ':consist-of-of', 'b']), [
+    'a',
+    ':consist',
+    'b',
+  ]);
+  t.deepEqual(m1.canonicalize(['a', ':mod', 'b']), ['a', ':mod', 'b']);
+  t.deepEqual(m1.canonicalize(['a', ':mod-of', 'b']), ['a', ':mod-of', 'b']);
+  t.deepEqual(m1.canonicalize(['a', ':domain', 'b']), ['a', ':domain', 'b']);
+  t.deepEqual(m1.canonicalize(['a', ':domain-of', 'b']), [
+    'a',
+    ':domain-of',
+    'b',
+  ]);
+  // without :
+  t.deepEqual(m1.canonicalize(['a', 'ARG0', 'b']), ['a', ':ARG0', 'b']);
+  t.deepEqual(m1.canonicalize(['a', 'ARG0-of', 'b']), ['a', ':ARG0-of', 'b']);
+  t.deepEqual(m1.canonicalize(['a', 'ARG0-of-of', 'b']), ['a', ':ARG0', 'b']);
+
+  const m2 = Model.fromDict(miniAmr());
+  t.deepEqual(m2.canonicalize(['a', ':ARG0', 'b']), ['a', ':ARG0', 'b']);
+  t.deepEqual(m2.canonicalize(['a', ':ARG0-of', 'b']), ['a', ':ARG0-of', 'b']);
+  t.deepEqual(m2.canonicalize(['a', ':ARG0-of-of', 'b']), ['a', ':ARG0', 'b']);
+  t.deepEqual(m2.canonicalize(['a', ':consist', 'b']), [
+    'a',
+    ':consist-of-of',
+    'b',
+  ]);
+  t.deepEqual(m2.canonicalize(['a', ':consist-of', 'b']), [
+    'a',
+    ':consist-of',
+    'b',
+  ]);
+  t.deepEqual(m2.canonicalize(['a', ':consist-of-of', 'b']), [
+    'a',
+    ':consist-of-of',
+    'b',
+  ]);
+  t.deepEqual(m2.canonicalize(['a', ':mod', 'b']), ['a', ':mod', 'b']);
+  t.deepEqual(m2.canonicalize(['a', ':mod-of', 'b']), ['a', ':domain', 'b']);
+  t.deepEqual(m2.canonicalize(['a', ':domain', 'b']), ['a', ':domain', 'b']);
+  t.deepEqual(m2.canonicalize(['a', ':domain-of', 'b']), ['a', ':mod', 'b']);
+  // without :
+  t.deepEqual(m2.canonicalize(['a', 'consist', 'b']), [
+    'a',
+    ':consist-of-of',
+    'b',
+  ]);
+  t.deepEqual(m2.canonicalize(['a', 'consist-of', 'b']), [
+    'a',
+    ':consist-of',
+    'b',
+  ]);
+  t.deepEqual(m2.canonicalize(['a', 'consist-of-of', 'b']), [
+    'a',
+    ':consist-of-of',
+    'b',
+  ]);
+});
+
+test('isRoleReifiable', (t) => {
+  const m1 = new Model();
+  t.false(m1.isRoleReifiable(':ARG0'));
+  t.false(m1.isRoleReifiable(':accompanier'));
+  t.false(m1.isRoleReifiable(':domain'));
+  t.false(m1.isRoleReifiable(':mod'));
+  const m2 = Model.fromDict(miniAmr());
+  t.false(m2.isRoleReifiable(':ARG0'));
+  t.true(m2.isRoleReifiable(':accompanier'));
+  t.false(m2.isRoleReifiable(':domain'));
+  t.true(m2.isRoleReifiable(':mod'));
+});
+
+test('reify', (t) => {
+  const m1 = new Model();
+  t.throws(() => m1.reify(['a', ':ARG0', 'b']));
+  t.throws(() => m1.reify(['a', ':accompanier', 'b']));
+  t.throws(() => m1.reify(['a', ':domain', 'b']));
+  t.throws(() => m1.reify(['a', ':mod', 'b']));
+  const m2 = Model.fromDict(miniAmr());
+  t.throws(() => m2.reify(['a', ':ARG0', 'b']));
+  t.deepEqual(m2.reify(['a', ':accompanier', 'b']), [
+    ['_', ':ARG0', 'a'],
+    ['_', ':instance', 'accompany-01'],
+    ['_', ':ARG1', 'b'],
+  ]);
+  t.throws(() => m2.reify(['a', ':domain', 'b']));
+  t.deepEqual(m2.reify(['a', ':mod', 'b']), [
+    ['_', ':ARG1', 'a'],
+    ['_', ':instance', 'have-mod-91'],
+    ['_', ':ARG2', 'b'],
+  ]);
+  // ensure unique ids if variables is specified
+  t.deepEqual(m2.reify(['a', ':mod', 'b'], new Set(['a', 'b', '_'])), [
+    ['_2', ':ARG1', 'a'],
+    ['_2', ':instance', 'have-mod-91'],
+    ['_2', ':ARG2', 'b'],
+  ]);
+});
+
+test('isConceptDereifiable', (t) => {
+  const m1 = new Model();
+  t.false(m1.isConceptDereifiable('chase-01'));
+  t.false(m1.isConceptDereifiable(':mod'));
+  t.false(m1.isConceptDereifiable('have-mod-91'));
+  const m2 = Model.fromDict(miniAmr());
+  t.false(m2.isConceptDereifiable('chase-01'));
+  t.false(m2.isConceptDereifiable(':mod'));
+  t.true(m2.isConceptDereifiable('have-mod-91'));
+});
+
+test('dereify', (t) => {
+  // (a :ARG1-of (_ / age-01 :ARG2 b)) -> (a :age b)
+  const t1: BasicTriple = ['_', ':instance', 'have-mod-91'];
+  const t1b: BasicTriple = ['_', ':instance', 'chase-01'];
+  const t2: BasicTriple = ['_', ':ARG1', 'a'];
+  const t3: BasicTriple = ['_', ':ARG2', 'b'];
+  const m1 = new Model();
+  t.throws(() => (m1 as any).dereify(t1));
+  t.throws(() => (m1 as any).dereify(t1, t2));
+  t.throws(() => (m1 as any).dereify(t1, t2, t3));
+  const m2 = Model.fromDict(miniAmr());
+  t.deepEqual(m2.dereify(t1, t2, t3), ['a', ':mod', 'b']);
+  t.deepEqual(m2.dereify(t1, t3, t2), ['a', ':mod', 'b']);
+  t.throws(() => m2.dereify(t1b, t2, t3));
+});
+
+test('errors', (t) => {
+  const m = new Model();
+  const a = Model.fromDict(miniAmr());
+  // basic roles
+  let g = new Graph([['a', ':instance', 'alpha']]);
+  t.deepEqual(m.errors(g), {});
+  g = new Graph([
+    ['a', ':instance', 'alpha'],
+    ['a', ':mod', '1'],
+  ]);
+  t.deepEqual(m.errors(g), {
+    [['a', ':mod', '1'].toString()]: ['invalid role'],
+  });
+  t.deepEqual(a.errors(g), {});
+  // regex role names
+  g = new Graph([
+    ['n', ':instance', 'name'],
+    ['n', ':op1', 'Foo'],
+    ['n', ':op2', 'Bar'],
+  ]);
+  t.deepEqual(a.errors(g), {});
+  // disconnected graph
+  g = new Graph([
+    ['a', ':instance', 'alpha'],
+    ['b', ':instance', 'beta'],
+  ]);
+  t.deepEqual(m.errors(g), {
+    [['b', ':instance', 'beta'].toString()]: ['unreachable'],
+  });
+  t.deepEqual(a.errors(g), {
+    [['b', ':instance', 'beta'].toString()]: ['unreachable'],
+  });
+});
---
+++ src/lib/model.ts
@@ -0,0 +1,503 @@
+/**
+ * Semantic models for interpreting graphs.
+ */
+
+import { ModelError } from './exceptions';
+import { CONCEPT_ROLE, Graph } from './graph';
+import { BasicTriple, Constant, Role, Target, Variable } from './types';
+
+type _ReificationSpec = [Role, Constant, Role, Role];
+type _Reified = [Constant, Role, Role];
+type _Dereified = [Role, Role, Role];
+type _Reification = [BasicTriple, BasicTriple, BasicTriple];
+
+/**
+ * A semantic model for Penman graphs.
+
+ * The model defines things like valid roles and transformations.
+
+ * Args:
+ *     top_variable: the variable of the graph's top
+ *     top_role: the role linking the graph's top to the top node
+ *     concept_role: the role associated with node concepts
+ *     roles: a mapping of roles to associated data
+ *     normalizations: a mapping of roles to normalized roles
+ *     reifications: a list of 4-tuples used to define reifications
+ */
+export class Model {
+  reifications: { [key: Role]: Array<_Reified> };
+  dereifications: { [key: Constant]: Array<_Dereified> };
+  topVariable: Variable;
+  topRole: Role;
+  conceptRole: Role;
+  roles: { [key: Role]: any };
+  normalizations: { [key: Role]: Role };
+  _roleRe: RegExp;
+
+  constructor(
+    topVariable: Variable = 'top',
+    topRole: Role = ':TOP',
+    conceptRole: Role = CONCEPT_ROLE,
+    roles: { [key: Role]: any } | null = null,
+    normalizations: { [key: Role]: Role } | null = null,
+    reifications: Array<_ReificationSpec> | null = null
+  ) {
+    this.topVariable = topVariable;
+    this.topRole = topRole;
+    this.conceptRole = conceptRole;
+
+    if (roles) {
+      roles = { ...roles };
+    }
+    this.roles = roles ? { ...roles } : {};
+    this._roleRe = new RegExp(
+      '^(' +
+        Object.keys(this.roles).concat([topRole, conceptRole]).join('|') +
+        ')$'
+    );
+
+    if (normalizations) {
+      normalizations = { ...normalizations };
+    }
+    this.normalizations = normalizations || {};
+
+    const reifs: { [key: Role]: Array<_Reified> } = {};
+    const deifs: { [key: Constant]: Array<_Dereified> } = {};
+    if (reifications) {
+      for (const [role, concept, source, target] of reifications) {
+        if (reifs[role] === undefined) {
+          reifs[role] = [];
+        }
+        reifs[role].push([concept, source, target]);
+        if (deifs[concept] === undefined) {
+          deifs[concept] = [];
+        }
+        deifs[concept].push([role, source, target]);
+      }
+    }
+    this.reifications = reifs;
+    this.dereifications = deifs;
+  }
+
+  equals(other: Model): boolean {
+    if (!(other instanceof Model)) {
+      return false;
+    }
+    return (
+      this.topVariable === other.topVariable &&
+      this.topRole === other.topRole &&
+      this.conceptRole === other.conceptRole &&
+      this.roles === other.roles &&
+      this.normalizations === other.normalizations &&
+      this.reifications === other.reifications
+    );
+  }
+
+  /**
+   * Instantiate a model from a dictionary.
+   */
+  static fromDict(d: { [key: string]: any }): Model {
+    return new Model(
+      d.topVariable,
+      d.topRole,
+      d.conceptRole,
+      d.roles,
+      d.normalizations,
+      d.reifications
+    );
+  }
+
+  /**
+   * Return ``True`` if *role* is defined by the model.
+   *
+   * If *role* is not in the model but a single deinversion of
+   * *role* is in the model, then ``True`` is returned. Otherwise
+   * ``False`` is returned, even if something like
+   * :meth:`canonicalize_role` could return a valid role.
+   */
+  hasRole(role: Role): boolean {
+    return (
+      this._hasRole(role) ||
+      (role.endsWith('-of') && this._hasRole(role.slice(0, -3)))
+    );
+  }
+
+  _hasRole(role: Role): boolean {
+    return this._roleRe.test(role);
+  }
+
+  /**
+   * Return ``True`` if *role* is inverted.
+   */
+  isRoleInverted(role: Role): boolean {
+    return !this._hasRole(role) && role.endsWith('-of');
+  }
+
+  /**
+   * Invert *role*.
+   */
+  invertRole(role: Role): Role {
+    if (!this._hasRole(role) && role.endsWith('-of')) {
+      return role.slice(0, -3);
+    }
+    return role + '-of';
+  }
+
+  /**
+   * Invert *triple*.
+   *
+   * This will invert or deinvert a triple regardless of its
+   * current state. :meth:`deinvert` will deinvert a triple only if
+   * it is already inverted. Unlike :meth:`canonicalize`, this will
+   * not perform multiple inversions or replace the role with a
+   * normalized form.
+   */
+  invert(triple: BasicTriple): BasicTriple {
+    const [source, role, target] = triple;
+    const inverse = this.invertRole(role);
+    // casting is just for the benefit of the type checker; it does
+    // not actually check that target is a valid variable type
+    return [target as Variable, inverse, source];
+  }
+
+  /**
+   * De-invert *triple* if it is inverted.
+   *
+   * Unlike :meth:`invert`, this only inverts a triple if the model
+   * considers it to be already inverted, otherwise it is left
+   * alone. Unlike :meth:`canonicalize`, this will not normalize
+   * multiple inversions or replace the role with a normalized
+   * form.
+   */
+  deinvert(triple: BasicTriple): BasicTriple {
+    if (this.isRoleInverted(triple[1])) {
+      triple = this.invert(triple);
+    }
+    return triple;
+  }
+
+  //     def canonicalize_role(self, role: Role) -> Role:
+  //         """
+  //         Canonicalize *role*.
+
+  //         Role canonicalization will do the following:
+
+  //         * Ensure the role starts with `':'`
+
+  //         * Normalize multiple inversions (e.g., ``ARG0-of-of`` becomes
+  //           ``ARG0``), but it does *not* change the direction of the role
+
+  //         * Replace the resulting role with a normalized form if one is
+  //           defined in the model
+  //         """
+  //         if role != '/' and not role.startswith(':'):
+  //             role = ':' + role
+  //         role = self._canonicalize_inversion(role)
+  //         role = self.normalizations.get(role, role)
+  //         return role
+
+  /**
+   * Canonicalize *role*.
+   *
+   * Role canonicalization will do the following:
+   *
+   * * Ensure the role starts with `':'`
+   *
+   * * Normalize multiple inversions (e.g., ``ARG0-of-of`` becomes
+   *   ``ARG0``), but it does *not* change the direction of the role
+   *
+   * * Replace the resulting role with a normalized form if one is
+   *   defined in the model
+   */
+  canonicalizeRole(role: Role): Role {
+    if (role !== '/' && !role.startsWith(':')) {
+      role = ':' + role;
+    }
+    role = this._canonicalizeInversion(role);
+    role = this.normalizations[role] || role;
+    return role;
+  }
+
+  //     def _canonicalize_inversion(self, role: Role) -> Role:
+  //         invert = self.invert_role
+  //         if not self._has_role(role):
+  //             while True:
+  //                 prev = role
+  //                 inverse = invert(role)
+  //                 role = invert(inverse)
+  //                 if prev == role:
+  //                     break
+  //         return role
+
+  _canonicalizeInversion(role: Role): Role {
+    if (!this._hasRole(role)) {
+      while (true) {
+        const prev = role;
+        const inverse = this.invertRole(role);
+        role = this.invertRole(inverse);
+        if (prev === role) {
+          break;
+        }
+      }
+    }
+    return role;
+  }
+
+  //     def canonicalize(self, triple: BasicTriple) -> BasicTriple:
+  //         """
+  //         Canonicalize *triple*.
+
+  //         See :meth:`canonicalize_role` for a description of how the
+  //         role is canonicalized. Unlike :meth:`invert`, this does not
+  //         swap the source and target of *triple*.
+  //         """
+  //         source, role, target = triple
+  //         canonical = self.canonicalize_role(role)
+  //         return (source, canonical, target)
+
+  /**
+   * Canonicalize *triple*.
+   *
+   * See :meth:`canonicalize_role` for a description of how the
+   * role is canonicalized. Unlike :meth:`invert`, this does not
+   * swap the source and target of *triple*.
+   */
+  canonicalize(triple: BasicTriple): BasicTriple {
+    const [source, role, target] = triple;
+    const canonical = this.canonicalizeRole(role);
+    return [source, canonical, target];
+  }
+
+  /**
+   * Return ``True`` if *role* can be reified.
+   */
+  isRoleReifiable(role: Role): boolean {
+    return role in this.reifications;
+  }
+
+  /**
+   * Return the three triples that reify *triple*.
+   *
+   * Note that, unless *variables* is given, the node variable
+   * for the reified node is not necessarily valid for the target
+   * graph. When incorporating the reified triples, this variable
+   * should then be replaced.
+ 
+   * If the role of *triple* does not have a defined reification, a
+   * :exc:`~penman.exceptions.ModelError` is raised.
+   *
+   * Args:
+   *     triple: the triple to reify
+   *     variables: a set of variables that should not be used for
+   *         the reified node's variable
+   * Returns:
+   *     The 3-tuple of triples that reify *triple*.
+   */
+  reify(triple: BasicTriple, variables?: Set<Variable>): _Reification {
+    const [source, role, target] = triple;
+    if (!(role in this.reifications)) {
+      throw new ModelError(`'${role}' cannot be reified`);
+    }
+    const [concept, sourceRole, targetRole] = this.reifications[role][0];
+
+    let variable = '_';
+    if (variables) {
+      let i = 2;
+      while (variables.has(variable)) {
+        variable = `_${i}`;
+        i += 1;
+      }
+    }
+
+    return [
+      [variable, sourceRole, source],
+      [variable, CONCEPT_ROLE, concept],
+      [variable, targetRole, target],
+    ];
+  }
+
+  /**
+   * Return ``True`` if *concept* can be dereified.
+   */
+  isConceptDereifiable(concept: Target): boolean {
+    return concept in this.dereifications;
+  }
+
+  /**
+   * Return the triple that dereifies the three argument triples.
+   *
+   * If the target of *instance_triple* does not have a defined
+   * dereification, or if the roles of *source_triple* and
+   * *target_triple* do not match those for the dereification of
+   * the concept, a :exc:`~penman.exceptions.ModelError` is
+   * raised. A :exc:`ValueError` is raised if *instance_triple* is
+   * not an instance triple or any triple does not have the same
+   * source variable as the others.
+   *
+   * Args:
+   *     instance_triple: the triple containing the node's concept
+   *     source_triple: the source triple from the node
+   *     target_triple: the target triple from the node
+   * Returns:
+   *     The triple that dereifies the three argument triples.
+   */
+  dereify(
+    instanceTriple: BasicTriple,
+    sourceTriple: BasicTriple,
+    targetTriple: BasicTriple
+  ): BasicTriple {
+    if (instanceTriple[1] !== CONCEPT_ROLE) {
+      throw new Error('second argument is not an instance triple');
+    }
+    if (
+      instanceTriple[0] !== sourceTriple[0] ||
+      instanceTriple[0] !== targetTriple[0]
+    ) {
+      throw new Error('triples do not share the same source');
+    }
+
+    const concept = instanceTriple[2];
+    const sourceRole = sourceTriple[1];
+    const targetRole = targetTriple[1];
+
+    if (!(concept in this.dereifications)) {
+      throw new ModelError(`${concept} cannot be dereified`);
+    }
+    for (const [role, source, target] of this.dereifications[concept]) {
+      if (source === sourceRole && target === targetRole) {
+        return [sourceTriple[2] as Variable, role, targetTriple[2]];
+      } else if (target === sourceRole && source === targetRole) {
+        return [targetTriple[2] as Variable, role, sourceTriple[2]];
+      }
+    }
+
+    throw new ModelError(
+      `${sourceRole} and ${targetRole} are not valid roles to dereify ${concept}`
+    );
+  }
+
+  /**
+   * Role sorting key that does not change the order.
+   */
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  originalOrder(_role: Role): boolean {
+    return true;
+  }
+
+  /**
+   * Role sorting key for alphanumeric order.
+   */
+  alphanumericOrder(role: Role): [string, number] {
+    const m = role.match(/(.*\D)(\d+)$/);
+    if (m) {
+      const rolename = m[1];
+      const roleno = parseInt(m[2], 10);
+      return [rolename, roleno];
+    } else {
+      return [role, 0];
+    }
+  }
+
+  /**
+   * Role sorting key that finds a canonical order.
+   */
+  canonicalOrder(role: Role): [boolean, [string, number]] {
+    return [this.isRoleInverted(role), this.alphanumericOrder(role)];
+  }
+
+  /**
+   * Role sorting key that randomizes the order.
+   */
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  randomOrder(_role: Role): number {
+    return Math.random();
+  }
+
+  /**
+   * Return a description of model errors detected in *graph*.
+
+   * The description is a dictionary mapping a context to a list of
+   * errors. A context is a triple if the error is relevant for the
+   * triple, or ``None`` for general graph errors.
+
+   * Example:
+
+   *     >>> from penman.models.amr import model
+   *     >>> from penman.graph import Graph
+   *     >>> g = Graph([('a', ':instance', 'alpha'),
+   *     ...            ('a', ':foo', 'bar'),
+   *     ...            ('b', ':instance', 'beta')])
+   *     >>> for context, errors in model.errors(g).items():
+   *     ...     print(context, errors)
+   *     ...
+   *     ('a', ':foo', 'bar') ['invalid role']
+   *     ('b', ':instance', 'beta') ['unreachable']
+   */
+  errors(graph: Graph): { [key: string]: string[] } {
+    const err: { [key: string]: string[] } = {};
+    if (graph.triples.length === 0) {
+      err[''] = ['graph is empty'];
+    } else {
+      const g: { [key: string]: BasicTriple[] } = {};
+      for (const triple of graph.triples) {
+        const [variable, role] = triple;
+        if (!this.hasRole(role)) {
+          err[triple.toString()] = ['invalid role'];
+        }
+        if (!(variable in g)) {
+          g[variable] = [];
+        }
+        g[variable].push(triple);
+      }
+      if (!graph.top) {
+        err[''] = ['top is not set'];
+      }
+      if (!(graph.top in g)) {
+        err[''] = ['top is not a variable in the graph'];
+      }
+      const reachable = _dfs(g, graph.top);
+      const unreachable = Object.keys(g).filter((k) => !reachable.has(k));
+      for (const uvar of unreachable) {
+        for (const triple of g[uvar]) {
+          err[triple.toString()] = ['unreachable'];
+        }
+      }
+    }
+
+    return err;
+  }
+}
+
+function _dfs(g: { [key: string]: BasicTriple[] }, top: string): Set<string> {
+  // just keep source and target of edge relations
+  const q: { [key: string]: Set<string> } = {};
+  for (const [variable, triples] of Object.entries(g)) {
+    q[variable] = new Set(
+      triples.map(([, , tgt]) => `${tgt}`).filter((tgt) => tgt in g)
+    );
+  }
+  // make edges bidirectional
+  for (const [variable, tgts] of Object.entries(q)) {
+    for (const tgt of tgts) {
+      if (!(tgt in q)) {
+        q[tgt] = new Set();
+      }
+      q[tgt].add(variable);
+    }
+  }
+
+  const visited = new Set<string>();
+  const agenda = [top];
+  while (agenda.length > 0) {
+    const cur = agenda.pop();
+    if (!visited.has(cur)) {
+      visited.add(cur);
+      for (const t of q[cur]) {
+        if (!visited.has(t)) {
+          agenda.push(t);
+        }
+      }
+    }
+  }
+  return visited;
+}
