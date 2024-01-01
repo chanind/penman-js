70eab9eaf2c6a5e7eff0b4cfd3f814647b19978d
David Chanin <chanindav@gmail.com>
2024-01-01 12:17:42 -0500

    chore: updating default color scheme for docs

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -49,6 +49,11 @@ const config: Config = {
   themeConfig: {
     // Replace with your project's social card
     // image: 'img/docusaurus-social-card.jpg',
+    colorMode: {
+      defaultMode: 'dark',
+      disableSwitch: false,
+      respectPrefersColorScheme: true,
+    },
     navbar: {
       title: 'Penman JS',
       // logo: {
